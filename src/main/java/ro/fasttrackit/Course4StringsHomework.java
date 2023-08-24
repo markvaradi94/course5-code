40ef7e40477395e059b6a089a85c59282ab1ee5d
markvaradi94 <mark.varadi03@gmail.com>
2023-08-24 21:22:55 +0300

    course5 code

---
+++ src/main/java/ro/fasttrackit/Course4StringsHomework.java
@@ -0,0 +1,50 @@
+package ro.fasttrackit;
+
+public class Course4StringsHomework {
+    public static void main(String[] args) {
+        System.out.println(countChar("test", 't'));
+        System.out.println(redact("test"));
+
+        String[] ana = {"Ana", "are", "mere"};
+        System.out.println(contains(ana, "mere"));
+
+        System.out.println(confidential("Ana are mere multe mere bune", new String[]{"mere", "Ana"}));
+    }
+
+    public static int countChar(String str, char c) {
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == c) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static String redact(String word) {
+        return "*".repeat(word.length());
+    }
+
+    public static boolean contains(String[] words, String str) {
+        for (String word : words) {
+            if (word.equals(str)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static String[] words(String phrase) {
+        return phrase.split(" ");
+    }
+
+    public static String confidential(String phrase, String[] sensitiveWords) {
+        String[] words = words(phrase);
+        for (int i = 0; i < words.length; i++) {
+            if (contains(sensitiveWords, words[i])) {
+                words[i] = redact(words[i]);
+            }
+        }
+        return String.join(" ", words);
+    }
+}
